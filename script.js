e9282f6bc7459ca71ef44f8b19358318e9d86436
seoyun1031 <2004leeyun@naver.com>
2024-12-22 00:56:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// 페이지 전환 효과
+// window.addEventListener('load', () => {
+//     // 3초 후에 메인 콘텐츠 표시
+//     setTimeout(() => {
+//         const intro = document.getElementById('intro');
+//         const mainContent = document.getElementById('main-content');
+
+//         // 인트로 화면 숨김
+//         intro.style.display = 'none';
+
+//         // 메인 콘텐츠 표시
+//         mainContent.style.display = 'block';
+//     }, 3000); // 3000ms = 3초
+// });
